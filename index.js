e09fb3e39a8939d2f163348670027dd824d5c0d5
Ismael Jr <ismaeljrdp@gmail.com>
2020-05-27 19:24:22 -0300

    Arquivo videos.js modificado

--- index.js
+++ index.js
@@ -6,45 +6,6 @@ formInput.addEventListener('submit',(el) => {
     console.log(el.target[0].value)
 })
 
-const videos = [
-    {
-        'name':'Video 01',
-        'author' : 'Steve',
-        'subscribes':200560,
-        'slug' :'56s4d29',
-        'date' : '2018-10-02',
-        'views' : 9892,
-        'likes' : 1560,
-        'deslikes' : 125,
-        'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
-        'comments':{
-            'author':'Bea',
-            'time':'1 year ago',
-            'likes' : 2,
-            'deslikes' : 0,
-            'comment':'The best video!! Wool'
-        }
-    },
-    {
-        'name':'Video 02',
-        'author' : 'Brad',
-        'subscribes':845560,
-        'slug' :'56s4d29',
-        'date' : '2019-12-16',
-        'views' : 3412,
-        'likes' : 350,
-        'deslikes' : 10,
-        'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
-        'comments':{
-            'author':'Alice',
-            'time':'2 months ago',
-            'likes' : 5,
-            'deslikes' : 1,
-            'comment':'The best video!! Wool'
-        }
-    }
-]
-
 const setView = (id) => {
 
     for (const key in videos[id]) {
